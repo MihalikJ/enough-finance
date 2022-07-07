203fe3a0f2219ab3d8d5c4a8d73752b9a21785ba
MihalikJ <jojjom10@gmail.com>
2022-07-07 19:50:14 +0200

    Added Frontpage with Data

---
+++ pages/api/getTopTen.js
@@ -0,0 +1,21 @@
+export default function handler(req, res){
+    const getData = async () => {
+        // musi to byt v ` ` kvoli template literal
+        const response = await fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${process.env.CMC_API_KEY}`, 
+        {
+            method: 'GET',
+            headers: {
+                Accept: '*/*'
+            },
+        },
+
+        )
+
+        const data = await response.json()
+
+        res.status(200).json({data})
+    }
+
+    getData()
+
+}
