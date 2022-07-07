793d4790e6cec2f9e5a3fa464ce4119bf043925f
MihalikJ <jojjom10@gmail.com>
2022-07-07 20:02:32 +0200

    Fixed MoralisProvider in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 import { MoralisProvider } from 'react-moralis'
 import { CoinMarketProvider } from '../context/context'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return (
     <MoralisProvider
-    serverUrl={process.env.NEXT_PUBLIC_SERVER}
-    appId={process.env.NEXT_PUBLIC_APP_ID}
+    serverUrl='https://zuaazrtgnflo.usemoralis.com:2053/server'
+    //serverUrl={process.env.NEXT_PUBLIC_SERVER}
+    appId='iCPLDbP0Ttg9pWZCaTgD4egVif2IYsHjcv2fZnyn'
+    //appId={process.env.NEXT_PUBLIC_APP_ID}
     >
       <CoinMarketProvider>
         <Component {...pageProps} />
